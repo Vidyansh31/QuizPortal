20a755d4b46c90a6de19f13f8e8434b4440fe83c
Vidyansh31 <deva5084@gmail.com>
2023-08-06 14:30:33 +0530

    Initial Setup done

---
+++ src/Components/FromBuilder.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const FormBuilder = () => {
+  return (
+    <div className="h-screen flex p-20 bg-gray-100">
+      <div className="w-70vw max-w-l border-t-4 border-purple-500 rounded shadow bg-white p-4">
+        <h1 className="text-5xl text-center mb-10 p-2 underline">Form Builder</h1>
+        {/* Other form components */}
+
+        <div className="w-60vw h-1/2 bg-red-500 mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque possimus veniam soluta earum molestiae fugit et dolorem modi voluptates similique magni excepturi blanditiis perspiciatis voluptate recusandae consequatur, consectetur ullam, at neque dolore. Architecto ipsam laborum adipisci quidem saepe consequuntur, id dolor debitis, totam ducimus laboriosam aliquid numquam aut asperiores esse illo quibusdam? Quisquam culpa qui error veniam iusto pariatur odit debitis fugit, reiciendis sint a exercitationem magnam, officia alias cumque in vero quas eligendi beatae mollitia voluptatibus harum. A, fugit possimus! A voluptatibus quod nam blanditiis placeat in laborum quasi aspernatur dolore eaque ipsum nobis pariatur, eum saepe rerum quos?</div>
+      <div className="w-60vw h-1/2 bg-orange-500 mt-4"></div>
+      <div className="w-60vw h-1/2 bg-blue-500 mt-4"></div>
+      </div>
+    </div>
+  )
+}
+
+export default FormBuilder
